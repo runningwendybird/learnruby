eee1064b2f42a8d3c98184f72c3ff76e83e49a7a
Hayley Denbraver <runningwendybird@gmail.com>
2015-04-17 13:13:28 -0700

    excercise 4 and 5 added.

---
+++ ex05.rb
@@ -0,0 +1,16 @@
+my_name = 'Hayley Denbraver'
+my_age = 29 
+my_height = 64 #inches
+my_weight = 150 #weight in pounds of a baby giraffe. Not my real weight.
+my_eyes = 'Blue'
+my_teeth = "White"
+my_hair = "Brown"
+
+puts "Let's talk about #{my_name}."
+puts "She's #{my_height} inches tall."
+puts "She's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "She's got #{my_eyes} eyes and #{my_hair} hair."
+puts "Her teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
