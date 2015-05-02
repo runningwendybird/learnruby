cbccf4455389c7fd86478afc05c62b56fc5acd99
Hayley Denbraver <runningwendybird@gmail.com>
2015-05-01 21:15:44 -0700

    Excercises 21 and 24.

---
+++ ex21.rb
@@ -0,0 +1,31 @@
+def add(a, b)
+	puts "ADDING #{a} + #{b}"
+	return a + b
+end
+
+def subtract(a, b)
+	puts "Subtracting #{a} - #{b}"
+	return a - b
+end
+
+def multiply(a, b) 
+	puts "MULTIPLYING #{a} * #{b}"
+	return a * b
+end
+
+def divide(a, b)
+	puts "DIVIDING #{a} / #{b}"
+	return a / b
+end
+
+puts "Let's do some math with just functions"
+
+# not my stats, fyi
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq} "
+
